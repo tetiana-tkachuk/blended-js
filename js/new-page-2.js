4ad0f5bc18268fd8a8af2bb9c3d36ac7c45fe5ec
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2026-01-08 21:55:28 +0200

    Update page-2

--- js/new-page-2.js
+++ js/new-page-2.js
@@ -261,3 +261,14 @@ function handleFruitClick(e) {
   position += shift;
   fruitBox.style.transform = `translate(${position}px, ${position}px)`;
 }
+
+/// 2 3 5 4 1
+setTimeout(() => console.log(1), 0);
+console.log(2);
+new Promise(res => {
+  console.log(3);
+  res();
+}).then(() => console.log(4));
+console.log(5);
+
+//
